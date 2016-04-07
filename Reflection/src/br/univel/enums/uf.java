78196c30682b6769b57b62cd607cac97ff0e6559
EduardoHFreitas <eduardo.freitas@datacoper.com.br>
2016-04-06 22:24:48 -0300

    Bem loco

---
+++ Reflection/src/br/univel/enums/uf.java
@@ -0,0 +1,15 @@
+package br.univel.enums;
+
+public enum uf {
+	PR(1, "Parana"),
+	SC(2, "Santa Catarina"),
+	RS(3, "Rio Grande Do Sul");
+	
+	private int id;
+	private String nome;
+	
+	private uf(int _id, String _nome){
+		this.id = _id;
+		this.nome = _nome;
+	}
+}
